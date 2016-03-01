b7eb3e8ec2adeca1b0c3f82b77de4f99d8e5170e
Shaun Osborne <shaun@cybergate9.net>
2016-03-01 09:19:08 +0000

    esimport tools added

--- esimport.sh
+++ esimport.sh
@@ -3,5 +3,5 @@
 # curl -XPUT http://52.16.145.22:9200/test-sedgwick/
 # curl -XPUT http://131.111.22.70:9200/sedgwick/id/_mapping --data-binary @esobjectmappings.json
 curl -XDELETE http://192.168.16.67:9200/sedgwick/$1/
-CURL -XPUT http://192.168.16.67:9200/sedgwick/$1/_mapping --data-binary @esmapping2.txt
-CURL -XPUT http://192.168.16.67:9200/sedgwick/_bulk --data-binary @$1-esbulk.txt
+curl -XPUT http://192.168.16.67:9200/sedgwick/$1/_mapping --data-binary @esmapping2.txt
+curl -XPUT http://192.168.16.67:9200/sedgwick/_bulk --data-binary @$1-esbulk.txt
